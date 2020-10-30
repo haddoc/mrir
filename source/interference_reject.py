f3c1ea96bdf58aa6ab82bc4d11f3d31a954a99fc
Unknown <hdyvorne@gmail.com>
2020-10-30 15:31:37 -0400

    Change dim order in rejection class

--- source/interference_reject.py
+++ source/interference_reject.py
@@ -18,42 +18,44 @@ class InterferenceReject(object):
         """Perform interference rejection
         Assumes Cartesian data, and performs spectral interference removal on the 3rd dimension
         :kwarg data_signal: the main imaging scan data 
-            time domain (lines, channels, readout)
+            time domain data of dimensions (channels, readout, lines)
         :kwarg data_noise: (optional) prescan data acquired without signal 
-            time domain (lines, channels, readout)
+            time domain of dimensions (channels, readout, lines)
         """
         assert isinstance(data_signal, np.ndarray)
         assert isinstance(data_noise, np.ndarray)
-        
+
         # Get scan data to spectral domain
-        spec_scan = np.fft.fft(data_signal, axis=-1)
-        
+        spec_scan = np.fft.fft(data_signal, axis=1)
+
         # Get noise data to spectral domain
-        spec_noise = np.fft.fft(data_noise, axis=-1)
+        spec_noise = np.fft.fft(data_noise, axis=1)
 
         # Compute coefficients using noise scan
         coeff_all = self.compute_coeffs(spec_noise)
-        
+
         # Perform rejection
-        num_freq_bins = spec_scan.shape[-1]
+        num_freq_bins = spec_scan.shape[1]
         ndims_output = list(spec_scan.shape)
-        ndims_output[1] = len(self.channels_signal)
+        ndims_output[0] = len(self.channels_signal)
         spec_cor = np.zeros(ndims_output, dtype=np.complex64)
         for _f in range(num_freq_bins):
-            sig = spec_scan[:, self.channels_signal, _f]
-            ref = spec_scan[:, self.channels_noise, _f]
-            spec_cor[:, :, _f] = sig - np.dot(ref, coeff_all[:, :, _f])
+            sig = spec_scan[self.channels_signal, _f, :]
+            ref = spec_scan[self.channels_noise, _f, :]
+            spec_cor[:, _f, :] = sig - np.dot(coeff_all[:, :, _f], ref)
 
-        data_cor = np.fft.ifft(spec_cor, axis=-1)
+        data_cor = np.fft.ifft(spec_cor, axis=1)
         return data_cor
 
     def compute_coeffs(self, cal_data):
         """Compute the correction coefficients for each frequency"""
-        num_freq_bins = cal_data.shape[-1]
-        coeff_all = np.zeros((len(self.channels_noise), len(self.channels_signal), num_freq_bins), dtype=np.complex64)
+        num_freq_bins = cal_data.shape[1]
+        coeff_all = np.zeros((len(self.channels_signal), len(self.channels_noise), num_freq_bins), dtype=np.complex64)
         for _f in range(num_freq_bins):
-            cal_sig = cal_data[:, self.channels_signal, _f]
-            cal_ref = cal_data[:, self.channels_noise, _f]
-            coeff = np.linalg.lstsq(cal_ref, cal_sig, rcond=-1)[0]
-            coeff_all[:, :, _f] = coeff
+            cal_sig = cal_data[self.channels_signal, _f, :]
+            cal_ref = cal_data[self.channels_noise, _f, :]
+            coeff = np.linalg.lstsq(cal_ref.T, cal_sig.T, rcond=-1)[0]
+            if coeff.ndim == 1:
+                coeff.expand_dims(axis=-1)
+            coeff_all[:, :, _f] = coeff.T
         return coeff_all
